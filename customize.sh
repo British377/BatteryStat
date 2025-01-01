111f046627b65c6e6b112d4c1f0464c31e0761fd
Rinker <75581950+British377@users.noreply.github.com>
2025-01-01 23:47:54 +0300

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -14,15 +14,4 @@ set_permissions() {
 set_permissions
 echo -e "┃\n┣ Setting permissions successfully..\n┃"
 
-echo -e "┃\n┣ Checking for indexes file...\n┃"
-
-indexes_file=$(find /data -type f -name "INT.INDEXES" -print -quit)
-
-if [ -n "$indexes_file" ]; then
-    echo "┃ indexes file found: $indexes_file"
-else
-    echo "┃┗ Error: indexes file not found"
-    exit 1
-fi
-
 echo -e "┃\n┗ Initialization script completed successfully!\n"
